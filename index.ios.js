2394f2fbbbd16092511d1834b5018b0076032a73
Brandon Everett <beverett@holtecmfg.com>
2016-11-11 15:24:33 -0500

    seperate index for ios and android

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,77 @@
 // index.ios.js
-// index.android.js
 
-import React, { AppRegistry } from 'react-native';
-import CodeSharing from './app';
+var React = require('react');
+var ReactNative = require('react-native');
+var {
+  AppRegistry,
+  TabBarIOS,
+  StyleSheet,
+  Text,
+  View,
+  Platform
+} = ReactNative;
+
+var HomeTab = require('./app/Tabs/HomeTab');
+var VideoTab = require('./app/Tabs/VideoTab');
+var AboutTab = require('./app/Tabs/AboutTab');
+
+export default class TheTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedTab: 'homeTab'
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <TabBarIOS tintColor="white" barTintColor="darkslateblue">
+          <TabBarIOS.Item
+              title="Home"
+              selected={this.state.selectedTab === 'homeTab'}
+              onPress={() => {
+            this.setState({selectedTab: 'homeTab'});
+          }}
+          >
+            <HomeTab/>
+          </TabBarIOS.Item>
+
+          <TabBarIOS.Item
+              title="Videos"
+              selected={this.state.selectedTab === 'videoTab'}
+              onPress={() => {
+            this.setState({selectedTab: 'videoTab'});
+          }}
+          >
+            <VideoTab/>
+          </TabBarIOS.Item>
+
+          <TabBarIOS.Item
+              title="About"
+              selected={this.state.selectedTab === 'aboutTab'}
+              onPress={() => {
+            this.setState({selectedTab: 'aboutTab'});
+          }}
+          >
+            <AboutTab/>
+          </TabBarIOS.Item>
+
+        </TabBarIOS>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
+});
 
 AppRegistry.registerComponent('TheTable', () => TheTable);
