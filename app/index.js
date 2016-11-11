85ef9f9696e792821ed9109907b088270b817916
Brandon Everett <beverett@holtecmfg.com>
2016-11-11 14:04:29 -0500

    create tab components for each tab

--- app/index.js
+++ app/index.js
@@ -8,15 +8,21 @@ var {
   View,
   Platform
 } = ReactNative;
+
 import Tabs from 'react-native-tabs';
+
 var YouTube = require('react-native-youtube');
+var HomeTab = require('./Tabs/HomeTab');
+var VideoTab = require('./Tabs/VideoTab');
+var AboutTab = require('./Tabs/AboutTab');
+
 
 export default class TheTable extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      page: 'First',
+      page: 'homeTab',
       isReady: false,
       status: null,
       quality: null,
@@ -30,28 +36,14 @@ export default class TheTable extends React.Component {
      const tabbarStyles = [styles.tabbar];
     if (Platform.OS === 'android') tabbarStyles.push(styles.androidTabbar);
     var tabInput;
-    if(page === 'First') {
-      tabInput = <Text> First</Text>;
+    if(page === 'homeTab') {
+      tabInput = <HomeTab />;
     }
-    else if (page ==='Second'){
-        tabInput = (<YouTube
-            ref="youtubePlayer"
-            videoId="Q0TohHXWtis" // The YouTube video ID
-            play={this.state.isPlaying}           // control playback of video with true/falseyout
-            hidden={false}        // control visiblity of the entire view
-            playsInline={true}    // control whether the video should play inline
-            loop={false}          // control whether the video should loop when ended
-            onReady={(e)=>{this.setState({isReady: true})}}
-            onChangeState={(e)=>{this.setState({status: e.state})}}
-            onChangeQuality={(e)=>{this.setState({quality: e.quality})}}
-            onError={(e)=>{this.setState({error: e.error})}}
-            onProgress={(e)=>{this.setState({currentTime: e.currentTime, duration: e.duration})}}
-            apiKey="AIzaSyBMzZ7Zb3WDJucbRX10Q1fGbfFO4xMwO3o"
-            style={{alignSelf: 'stretch', height: 300, backgroundColor: 'black', marginVertical: 10}}
-                    />);
+    else if (page ==='videosTab'){
+        tabInput = <VideoTab />;
     }
     else {
-        tabInput = <Text>Third </Text>;
+        tabInput = <AboutTab/>;
     }
     return (
       <View style={styles.container}>
@@ -60,9 +52,9 @@ export default class TheTable extends React.Component {
             style={tabbarStyles}
             selectedStyle={{color:'blue'}} onSelect={el=>this.setState({page:el.props.name})}
         >
-            <Text name="First">First</Text>
-            <Text name="Second">Second</Text>
-            <Text name="Third">Third</Text>
+            <Text name="homeTab">Home</Text>
+            <Text name="videosTab">Videos</Text>
+            <Text name="aboutTab">About</Text>
         </Tabs>
 
           {tabInput}
