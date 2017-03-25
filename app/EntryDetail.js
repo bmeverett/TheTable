47e7c67af1d44a49841bece34f8db09ac6d70347
Brandon Everett <bme2010@gmail.com>
2017-03-25 16:18:28 -0400

    get images to work on entry detail

--- app/EntryDetail.js
+++ app/EntryDetail.js
@@ -1,35 +1,51 @@
 import React from 'react';
-import ReactNative, { StyleSheet, View, WebView } from 'react-native';
+import ReactNative, { Image, StyleSheet, View, WebView } from 'react-native';
+
+var safeHtml = require('safe-html');
+var config = {
+  allowedTags: ['strong', 'span', 'b', 'p', 'i', 'em'],
+  allowedAttributes: {
+  },
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: 'white',
   },
-  title: {
-    paddingTop: 2,
-    paddingBottom: 3,
-    paddingRight: 15,
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
   webView: {
+    flex: 1,
   },
 });
 
 export default class EntryDetail extends React.Component {
   render() {
     // console.log(this.props.entry);
+    const img = this.props.entry.body.split('<img');
+    let santitize = safeHtml(this.props.entry.body, config);
+    const test = santitize.replace('&lt;', '<').replace('&quot;', '"').replace('/&gt;', '>').replace('&quot; ', '"');
+    santitize = safeHtml(test, config).trim();
+    console.log(santitize);
+    let imageSource;
+    if (img.length > 1) {
+      img[1].split('=');
+      const index = img[1].indexOf('>');
+      const realHtml = img[1].substring(0, index - 1).split('"')[1];
+      imageSource = <Image style={{ flex: 1, alignSelf: 'auto' }} resizeMode= 'contain' source={{ uri: realHtml }} />;
+    }
     return (
       <View style={styles.container}>
+        {imageSource}
+        <View style={{ flex: 1, padding: 0 }}>
         <WebView
           automaticallyAdjustContentInsets
-          source={{ html: this.props.entry.body }}
+          source={{ html: santitize }}
           javaScriptEnabled
           onNavigationStateChange={this.onNavigationStateChange}
         />
+        </View>
       </View>
+     
     );
   }
 }
