336ed9661062c2ea0a6001516ca6bc8a56b0fab9
Brandon Everett <bme2010@gmail.com>
2017-04-09 15:30:57 -0400

    update notes tab to scroll with text

--- app/Tabs/Notes.js
+++ app/Tabs/Notes.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { TextInput, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import ReactNative, { TextInput, View, TouchableWithoutFeedback, Keyboard, ScrollView } from 'react-native';
 import Api from '../Api/RssFeedApi';
 import parseImage from '.././ParseImage';
 
 export default class Notes extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: this.props.text, subject: this.props.subject, imgSource: null };
+    this.state = {
+      text: this.props.text,
+      subject: this.props.subject,
+      imgSource: null,
+      textObj: null };
   }
 
   componentDidMount() {
@@ -18,31 +22,44 @@ export default class Notes extends React.Component {
       this.setState({ imgSource: img });
     }).catch(error => console.log(error));
   }
-
+  inputFocused(refName) {
+    setTimeout(() => {
+      const scrollResponder = this.refs.scrollView.getScrollResponder();
+      scrollResponder.scrollResponderScrollNativeHandleToKeyboard(
+      ReactNative.findNodeHandle(this.refs[refName]),
+      0, // additionalOffset
+      true
+    );
+    }, 50);
+  }
   render() {
     return (
-       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-         <View style={{ flex: 1, paddingTop: 60 }} >
-           <View style={{ flex: 1 }} >
-             {this.state.imgSource}
-           </View>
-           <TextInput
-             placeholder="Subject"
-             style={{ margin: 5, height: 40, fontSize: 14 }}
-             onChangeText={this.props.onSubjectChange}
-             value={this.props.subject}
-             editable
-           />
-           <TextInput
-             placeholder="Notes"
-             style={{ margin: 5, flex: 1, fontSize: 14, marginBottom: 100, marginRight: 50 }}
-             onChangeText={this.props.onChangeText}
-             value={this.props.text}
-             editable
-             multiline
-           />
-         </View>
-       </TouchableWithoutFeedback>
+      <ScrollView ref='scrollView' contentContainerStyle={{ flex: 1 }} >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={{ flex: 1, paddingTop: 20 }} >
+            {this.state.imgSource}
+            <TextInput
+              placeholder="Subject"
+              style={{ margin: 5, height: 40, fontSize: 14 }}
+              onChangeText={this.props.onSubjectChange}
+              value={this.props.subject}
+              editable
+            />
+            <TextInput
+            // hack this for now, can fix with propper refs later
+              ref='notes'
+              onFocus={this.inputFocused.bind(this, 'notes')}
+              placeholder="Notes"
+              style={{ margin: 5, flex: 1, fontSize: 14, marginBottom: 100, marginRight: 50 }}
+              onChangeText={this.props.onChangeText}
+              value={this.props.text}
+              editable
+              multiline
+            />
+
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
     );
   }
 }
--- index.ios.js
+++ index.ios.js
@@ -54,7 +54,6 @@ export default class TheTable extends React.Component {
     }
   }
   render() {
-    console.log(this.state.reload);
     return (
       <TabBarIOS
         unselectedTintColor="black"
