0c0bd2441a9c61ea94a82237b556182b756d9251
Brandon Everett <bme2010@gmail.com>
2017-04-19 18:35:21 -0400

    update about tab

--- app/Tabs/About/Times.js
+++ app/Tabs/About/Times.js
@@ -4,12 +4,9 @@ import { View, Text } from 'react-native';
 export default function Times(props) {
   return (
     <View style={{ flex: 1, paddingTop: 10, paddingHorizontal: 10 }}>
-      <Text>
-        EACH WEEK WE SPEND TIME IN COMMUNITY AND DISCUSS A PASSAGE OF THE BIBLE. ON THE LAST TUESDAY OF EVERY MONTH WE EAT A FULL MEAL TOGETHER. 
-      </Text>
-      <Text>{'\n'}Times </Text>
-      <Text>{'\n'}Weekly: Tuesdays, 7PM - 9PM </Text>
-      <Text>{'\n'}Monthly Meal (last Tuesday of the month): 7PM - 9PM </Text>
+      <Text style={{ fontWeight: 'bold' }}>{'\n'}Times </Text>
+      <Text>{'\n'}Weekly: Tuesdays, 7PM - 8:30PM </Text>
+      <Text>{'\n'}Family Meal (last Tuesday of the month): 7PM - 8:30PM </Text>
     </View>
   );
 }
