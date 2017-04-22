3d9f35ce9fbf13692c985c22e2c2ae3d6b199452
Brandon Everett <bme2010@gmail.com>
2017-04-22 18:07:39 -0400

    update snapshots and fastlane metadata

--- ios/TheTableUITests/TheTableUITests.swift
+++ ios/TheTableUITests/TheTableUITests.swift
@@ -37,7 +37,7 @@ class TheTableUITests: XCTestCase {
       let app = XCUIApplication()
       XCUIDevice.shared().orientation = .portrait
       snapshot("01Home")
-      app.otherElements["     3P // Part 1  Tue Apr 04 2017    My Side of Iron // Part 3 Tue Mar 21 2017    My Side of Iron // Part 2 Tue Mar 14 2017    My Side of Iron // Part 1 Tue Mar 07 2017    Family Meal  Tue Feb 28 2017    You Can't Teach Hunger / / Part 3  Tue Feb 21 2017    You Can't Teach Hunger / / Part 2  Mon Feb 13 2017     Tue Feb 07 2017    Family Meal  Tue Jan 31 2017    The New Now // Part 4 Tue Jan 24 2017    The New Now // Part 2 Tue Jan 10 2017    The New Now // Part 1 Tue Jan 03 2017    The Table 12/13/16 Tue Dec 13 2016    Person is Personal // Week 2  Tue Dec 13 2016     Tue Dec 06 2016     Tue Nov 29 2016    Where is the love? / / Part 3 Tue Nov 15 2016    Where is the love? / / Part 2  Tue Nov 08 2016     Tue Nov 01 2016     Tue Oct 25 2016"].otherElements["   3P // Part 1  Tue Apr 04 2017"].tap()
+      XCUIApplication().otherElements["  TONIGHT "].tap()
       snapshot("02Detail")
       app.tabBars.buttons["Videos"].tap()
       snapshot("03Videos")
@@ -45,6 +45,8 @@ class TheTableUITests: XCTestCase {
       snapshot("04Notes")
       app.tabBars.buttons["About"].tap()
       snapshot("05About")
+      
+      
       
       
     }
