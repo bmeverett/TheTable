8ea372bf426c39555b1a613842c360486bfc4ffb
Brandon Everett <bme2010@gmail.com>
2017-08-17 16:32:59 -0400

    upgradde rn version

--- android/app/src/main/java/com/thetable/MainApplication.java
+++ android/app/src/main/java/com/thetable/MainApplication.java
@@ -1,7 +1,6 @@
 package com.thetable;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
