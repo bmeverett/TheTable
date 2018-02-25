b047229ce39fa8f4bd057f941d4decc5003417a1
Brandon Everett <bme2010@gmail.com>
2018-02-25 09:02:03 -0500

    fix image when not live

--- app/LiveView.js
+++ app/LiveView.js
@@ -10,7 +10,7 @@ export default class LiveView extends React.Component {
       ctrl = <View style={{ flex: 1 }} ><WebView style={{ flex: 1 }} source={{ uri: 'https://thetable.churchonline.org' }} /></View>;
     } else {
       ctrl = (<View style={{ flex: 1, backgroundColor: '#8c8b8a', alignItems: 'center' }} >
-        <Image style={{ flex: 1, alignItems: 'center', justifyContent: 'center', alignItems: 'center' }} resizeMode="center" source={require('./images/tablelive.png')} />
+        <Image style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} resizeMode="center" source={require('./images/tablelive.png')} />
       </View>);
     }
     return (
