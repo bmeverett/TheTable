e1d510a44ad9092d1ef7815e0a3c1e9ab8a305fd
Brandon Everett <bme2010@gmail.com>
2018-03-07 12:42:36 -0500

    add loading indicator for GivingTab

--- app/Tabs/GivingTab.js
+++ app/Tabs/GivingTab.js
@@ -3,7 +3,8 @@ import { WebView } from 'react-native';
 
 function GivingTab() {
   return <WebView
-    source={{ uri: 'https://www.eservicepayments.com/cgi-bin/Vanco_ver3.vps?appver3=QYgs3QBcu8xbOrLktBLPzvKXho7Z1f-vPAmlEy31-Mtxu1xOi9A1zOcBRvMIg3dWDcwKUSIIMplPYbhp_Bye2yBBvx9-fvMagEqoHNQ2OkpXglg96ix4LAXyIe06lRTbvIbrKP-ltHb6Y7F8Ylp0-44S31PvHyzCSPZoGMvkv08=&ver=3' }} 
+    source={{ url: 'https://www.eservicepayments.com/cgi-bin/Vanco_ver3.vps?appver3=QYgs3QBcu8xbOrLktBLPzvKXho7Z1f-vPAmlEy31-Mtxu1xOi9A1zOcBRvMIg3dWDcwKUSIIMplPYbhp_Bye2yBBvx9-fvMagEqoHNQ2OkpXglg96ix4LAXyIe06lRTbvIbrKP-ltHb6Y7F8Ylp0-44S31PvHyzCSPZoGMvkv08=&ver=3https://www.eservicepayments.com/cgi-bin/Vanco_ver3.vps?appver3=QYgs3QBcu8xbOrLktBLPzvKXho7Z1f-vPAmlEy31-Mtxu1xOi9A1zOcBRvMIg3dWDcwKUSIIMplPYbhp_Bye2yBBvx9-fvMagEqoHNQ2OkpXglg96ix4LAXyIe06lRTbvIbrKP-ltHb6Y7F8Ylp0-44S31PvHyzCSPZoGMvkv08=&ver=3' }}
+    startInLoadingState
   />;
 }
 
