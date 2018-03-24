f5a480519588ab92c55241ec6ccf74f19c3808d1
Brandon Everett <bme2010@gmail.com>
2018-03-24 18:46:02 -0400

    finish android push notifications

---
+++ android/app/src/main/java/com/thetable/FirebaseMessaging.java
@@ -0,0 +1,24 @@
+package com.thetable;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by brandoneverett on 3/10/18.
+ */
+
+public class FirebaseMessaging extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+
+    }
+}
