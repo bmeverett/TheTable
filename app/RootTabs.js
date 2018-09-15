744455c6197b06050d813424795d0acc610eeabe
Brandon Everett <bme2010@gmail.com>
2018-09-15 19:00:15 -0400

    fix navigation issues with upgrading react navigation

--- app/RootTabs.js
+++ app/RootTabs.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Alert, Button, Keyboard, Linking } from "react-native";
 import {
-  TabNavigator,
-  StackNavigator,
   createBottomTabNavigator,
   createStackNavigator
 } from "react-navigation";
@@ -16,128 +14,12 @@ import LiveView from "./LiveView";
 
 const giveUrl =
   "https://www.eservicepayments.com/cgi-bin/Vanco_ver3.vps?appver3=QYgs3QBcu8xbOrLktBLPzvKXho7Z1f-vPAmlEy31-Mtxu1xOi9A1zOcBRvMIg3dWDcwKUSIIMplPYbhp_Bye2yBBvx9-fvMagEqoHNQ2OkpXglg96ix4LAXyIe06lRTbvIbrKP-ltHb6Y7F8Ylp0-44S31PvHyzCSPZoGMvkv08=&ver=3https://www.eservicepayments.com/cgi-bin/Vanco_ver3.vps?appver3=QYgs3QBcu8xbOrLktBLPzvKXho7Z1f-vPAmlEy31-Mtxu1xOi9A1zOcBRvMIg3dWDcwKUSIIMplPYbhp_Bye2yBBvx9-fvMagEqoHNQ2OkpXglg96ix4LAXyIe06lRTbvIbrKP-ltHb6Y7F8Ylp0-44S31PvHyzCSPZoGMvkv08=&ver=3";
-const RootTabs = createBottomTabNavigator(
-  {
-    Home: {
-      screen: HomeTab,
-      navigationOptions: {
-        tabBarLabel: "Home",
-        title: "Home",
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={focused ? "ios-home" : "ios-home-outline"}
-            size={26}
-            style={{ color: tintColor }}
-          />
-        )
-      }
-    },
-    Videos: {
-      screen: LiveView,
-      navigationOptions: ({ navigation }) => ({
-        tabBarLabel: "Live",
-        title: "Live",
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Icon2
-            name={focused ? "social-youtube" : "social-youtube"}
-            size={26}
-            style={{ color: tintColor }}
-          />
-        )
-      })
-    },
-    Notes: {
-      screen: Notes,
-      navigationOptions: ({ navigation }) => ({
-        tabBarLabel: "Notes",
-        title: "Notes",
-        headerRight: (
-          <Button
-            title="Share"
-            onPress={() => {
-              let subject = navigation.state.params
-                ? navigation.state.params.subject
-                : "";
-              let text = navigation.state.params
-                ? navigation.state.params.text
-                : "";
 
-              Linking.canOpenURL(
-                `mailto:?subject=${subject}&body=${text}`
-              ).then(supported => {
-                if (supported) {
-                  Linking.openURL(`mailto:?subject=${subject}&body=${text}`);
-                } else {
-                  Alert.alert("Error", "Don't know how to open URI: ");
-                }
-              });
-              Keyboard.dismiss();
-            }}
-          />
-        ),
-        headerLeft: (
-          <Button
-            title="Done"
-            onPress={() => {
-              Keyboard.dismiss();
-            }}
-          />
-        ),
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={focused ? "ios-clipboard" : "ios-clipboard-outline"}
-            size={26}
-            style={{ color: tintColor }}
-          />
-        )
-      })
-    },
-    About: {
-      screen: AboutTab,
-      navigationOptions: {
-        tabBarLabel: "About",
-        title: "About",
-        headerRight: (
-          <Button
-            title="Give"
-            onPress={() => {
-              Linking.openURL(giveUrl);
-            }}
-          />
-        ),
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={
-              focused
-                ? "ios-information-circle-outline"
-                : "ios-information-circle-outline"
-            }
-            size={26}
-            style={{ color: tintColor }}
-          />
-        )
-      }
-    }
-  },
-  {
-    tabBarOptions: {
-      // activeTintColor: 'black',
-      // tabBarLabel: {
-      //   tintColor: 'black',
-      // },
-      // style: {
-      //   backgroundColor: 'gray',
-      // },
-    }
-  }
-);
-
-const Base = createStackNavigator({
+const HomeStack = createStackNavigator({
   Home: {
-    screen: RootTabs,
-    title: "Home",
+    screen: HomeTab,
     navigationOptions: () => ({
-      title: "Home"
+      headerTitle: "Home"
     })
   },
   EntryDetail: {
@@ -145,10 +27,119 @@ const Base = createStackNavigator({
     navigationOptions: ({ navigation }) => ({
       title: `${navigation.state.params.title}`
     })
-  },
-  LiveView: {
-    screen: LiveView
   }
 });
+
+const LiveStack = createStackNavigator({
+  Live: {
+    screen: LiveView,
+    navigationOptions: () => ({
+      headerTitle: "Live"
+    })
+  }
+});
+
+const NotesStack = createStackNavigator({
+  Notes: {
+    screen: Notes,
+    navigationOptions: ({ navigation }) => ({
+      tabBarLabel: "Notes",
+      title: "Notes",
+      headerRight: (
+        <Button
+          title="Share"
+          onPress={() => {
+            const subject = navigation.state.params
+              ? navigation.state.params.subject
+              : "";
+            const text = navigation.state.params
+              ? navigation.state.params.text
+              : "";
+
+            Linking.canOpenURL(`mailto:?subject=${subject}&body=${text}`).then(
+              supported => {
+                if (supported) {
+                  Linking.openURL(`mailto:?subject=${subject}&body=${text}`);
+                } else {
+                  Alert.alert("Error", "Don't know how to open URI: ");
+                }
+              }
+            );
+            Keyboard.dismiss();
+          }}
+        />
+      ),
+      headerLeft: (
+        <Button
+          title="Done"
+          onPress={() => {
+            Keyboard.dismiss();
+          }}
+        />
+      )
+    })
+  }
+});
+
+const AboutStack = createStackNavigator({
+  About: {
+    screen: AboutTab,
+    navigationOptions: () => ({
+      tabBarLabel: "About",
+      title: "About",
+      headerRight: (
+        <Button
+          title="Give"
+          onPress={() => {
+            Linking.openURL(giveUrl);
+          }}
+        />
+      )
+    })
+  }
+});
+
+const Base = createBottomTabNavigator(
+  {
+    Home: HomeStack,
+    Live: LiveStack,
+    Notes: NotesStack,
+    About: AboutStack
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = `ios-home${focused ? "" : "-outline"}`;
+        } else if (routeName === "Live") {
+          return (
+            <Icon2
+              name={focused ? "social-youtube" : "social-youtube"}
+              size={26}
+              style={{ color: tintColor }}
+            />
+          );
+        } else if (routeName === "Notes") {
+          iconName = `ios-clipboard${focused ? "" : "-outline"}`;
+        } else if (routeName === "About") {
+          return (
+            <Ionicons
+              name={
+                focused
+                  ? "ios-information-circle-outline"
+                  : "ios-information-circle-outline"
+              }
+              size={26}
+              style={{ color: tintColor }}
+            />
+          );
+        }
+        return <Ionicons name={iconName} size={26} color={tintColor} />;
+      }
+    })
+  }
+);
 
 export default Base;
